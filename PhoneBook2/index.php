768ce8085aa4986a2d0311336b955eac119a8dd1
rohitg00 <48523873+rohitg00@users.noreply.github.com>
2019-03-14 17:15:57 +0530

    Add files via upload
    PHONE DIRECTORY

---
+++ PhoneBook2/index.php
@@ -0,0 +1,31 @@
+<html>
+<link rel="stylesheet" href="my_layout.css"  type="text/css" />
+<title>Phone Directory Application</title>
+<body class="my_body">
+     
+	 <div id="my_divition">  
+   <form action="Login.php" method="post" >
+   <center>
+   <h1> PHONE DIRECTORY </h1>
+   <br />
+     <b>Username  </b> <br />
+	 <input type="text" name="username" size="10" /><br /><br />
+	 <b>Password  </b><br />
+	 <input type="password" name="password" size="10" /><br /><br />
+	 <input type="submit" value="Login"  />
+	  </form>  
+	   
+      <form action="Register.php" method="post" >
+	  <br />
+	  <br />
+	  <b> <u>To add new member</b></u><br /><br />
+	  	 <input type="submit" value="Register"  />
+	  </form>
+	 <br />
+	 <br />
+	 <br />
+ 
+  
+   </div>
+</body>
+</html>
